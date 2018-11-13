59bc4c8f22775a9593c1314ffd3cd4dedfbbf644
Arianna <aramirez@tedexis.com>
2018-11-13 19:36:20 -0400

    Separar en caracter cada item del arreglo de la grip

--- Algoritmos de búsqueda/pacmanbfs.py
+++ Algoritmos de búsqueda/pacmanbfs.py
@@ -1,22 +1,23 @@
 #Definicion de la clase vertice
 class Vertex:
-    def __init__(self,n):
-        self.name = n
+    def __init__(self,x,y,value):
+        self.x = x
+        self.y = y
         self.neighbors = list()
-
-        self.distance = 9999
+        self.value = value
         self.visited = False
     #Agregar a los vecinos
     def add_neighbor(self, v):
         if v not in self.neighbors:
             self.neighbors.append(v)
             #self.neighbors.sort()
-    def print_neighbor(self):
-        print(self.name, " Mis vecinos ",self.neighbors)          
+    def get(self):
+        print(self.x," ",self.y," ",self.value)
+    #def print_neighbor(self):
+        #print(self.name, " Mis vecinos ",self.neighbors)          
 
 
 #Lectura de variables
-#Coordenadas de Pacman
 
 pacman =  input().strip()
 comida =  input().strip()
@@ -26,15 +27,14 @@ coordenadas_pacman = pacman.split()
 coordenadas_comida = comida.split()
 size_mapa = mapa.split()
 
-x_pacman = coordenadas_pacman[0]
-y_pacman = coordenadas_pacman[1]
+x_pacman = int(coordenadas_pacman[0])
+y_pacman = int(coordenadas_pacman[1])
 
 x_comida = coordenadas_comida[0]
 y_comida = coordenadas_comida[1]
 
 rows_mapa = int(size_mapa[0])
 colums_mapa = size_mapa[1]
-
 
 grid=[]
 for i in range(rows_mapa):
@@ -49,4 +49,10 @@ print(rows_mapa)
 print(colums_mapa)
 print(grid)
 
+raiz = Vertex(x_pacman,y_pacman,'P')
+raiz.get()
+
+for x in grid:
+    print(x)
+    print(list(x))
 

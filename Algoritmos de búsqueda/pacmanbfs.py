aeeb7374648175bac33636d0cfc3d3aa2ba147f6
Arianna <aramirez@tedexis.com>
2018-11-14 17:59:52 -0400

    Pacman BFS Terminado

--- Algoritmos de búsqueda/pacmanbfs.py
+++ Algoritmos de búsqueda/pacmanbfs.py
@@ -1,12 +1,14 @@
 lista = list()
+queue = list()
 class Vertex:
     def __init__(self,x,y,value):
         self.x = x
         self.y = y
         self.value = value
+        self.papa  = list()
      
 def bfs(matriz,raiz,visitados,x_comida,y_comida):
-    queue = list()
+    
     raiz.visited =True
     visitados[raiz.x][raiz.y] = 1
     queue.append(raiz)
@@ -24,21 +26,25 @@ def bfs(matriz,raiz,visitados,x_comida,y_comida):
         if(visitados[elemento.x-1][elemento.y] != 1 and matriz[elemento.x-1][elemento.y] != "%"):
             b = Vertex(elemento.x-1,elemento.y,matriz[elemento.x-1][elemento.y])
             visitados[elemento.x-1][elemento.y] = 1
+            b.papa.append(elemento)
             queue.append(b)
        
         if(visitados[elemento.x][elemento.y-1] != 1 and matriz[elemento.x][elemento.y-1] != "%"):
             c = Vertex(elemento.x,elemento.y-1,matriz[elemento.x-1][elemento.y-1])
             visitados[elemento.x][elemento.y-1] = 1
+            c.papa.append(elemento)
             queue.append(c)
        
         if(visitados[elemento.x][elemento.y+1] != 1 and matriz[elemento.x][elemento.y+1] != "%"):
             d = Vertex(elemento.x,elemento.y+1,matriz[elemento.x][elemento.y+1])
             visitados[elemento.x][elemento.y+1] = 1
+            d.papa.append(elemento)
             queue.append(d)
    
         if(visitados[elemento.x+1][elemento.y] != 1 and matriz[elemento.x+1][elemento.y] != "%"):
             e = Vertex(elemento.x+1,elemento.y,matriz[elemento.x+1][elemento.y])
             visitados[elemento.x+1][elemento.y] = 1
+            e.papa.append(elemento)
             queue.append(e)
     return count
 
@@ -75,3 +81,26 @@ visitados = [[0 for col in range(colums_mapa)] for row in range(rows_mapa)]
 print(bfs(matrix,raiz,visitados,x_comida,y_comida))
 for elemento in lista:
     print(elemento.x,elemento.y)
+
+q = list()
+ultimo = lista[-1]
+q.append(ultimo)
+ultimo_papa = ultimo.papa
+final = len(ultimo_papa)
+index =  lista.index(ultimo_papa[0])
+
+q.append(lista[index])
+
+while index != 0:
+    ultimo = lista[index]
+    ultimo_papa = ultimo.papa
+    final = len(ultimo_papa)
+    index =  lista.index(ultimo_papa[0])
+    q.append(lista[index])
+
+print(len(q)-1)
+q.reverse()
+for elemento in q:
+    print(elemento.x,elemento.y)
+
+

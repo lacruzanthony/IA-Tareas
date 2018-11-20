e66834f2ca1b8d0f6b7739c1f6c417b0cb5e1a2c
fgurbano23 <fgurbano23@gmail.com>
2018-11-19 20:10:53 -0400

    enteros menores a 9

--- Algoritmos de búsqueda/conjeturadfs.py
+++ Algoritmos de búsqueda/conjeturadfs.py
@@ -7,39 +7,43 @@ tree = [
 bd = []
 bd.append(tree[0])
 
-reach = 8
+reach = 9
 stop = False
 
 def factorialAprox(n):
     #Aproximacion stirling a factoriales muy grandes
-    return n * math.log(n) - n
+    return math.log(n)
 
 def isStackble(value,op):
 
-
-    if not any(elm['value'] == math.sqrt(value) for elm in bd):
-        #ESTA ES LA CONDIFCION LOCA 
-        if ( math.floor(math.sqrt(value)) == reach ):
-            tree.insert(0,{'value':math.floor(math.sqrt(value)),'op':op + 'V-'})
-            bd.append({'value':math.floor(math.sqrt(value)), 'op':op + 'V-'})
-
-        tree.append({'value':math.sqrt(value), 'op':op + 'V'})
-        bd.append({'value':math.sqrt(value), 'op':op + 'V'})
-
-    if not any(elm['value'] == math.floor(value) for elm in bd):
-        tree.append({'value':math.floor(value),'op':op + '-'})
+    if (math.floor(value) == reach):
+        tree.insert(1,{'value':math.floor(value), 'op':op + '-'})
         bd.append({'value':math.floor(value), 'op':op + '-'})
 
-    if(value - math.trunc(value) == 0):
-        if not (abs(math.factorial(value)) > (2 ** 31 - 1)):
-            if not any(elm['value'] == math.factorial(value) for elm in bd):
-                tree.append({'value':math.factorial(value), 'op':op + '!'})
-                bd.append({'value':math.factorial(value),'op':op + '!'})
+    elif(math.sqrt(value) == reach):
+        tree.insert(1,{'value':math.sqrt(value), 'op':op + '-'})
+        bd.append({'value':math.sqrt(value), 'op':op + '-'})
 
-        elif (value < 100):
-            if not any(elm['value'] == factorialAprox(value) for elm in bd):
-                tree.append({'value':factorialAprox(value), 'op':op + '!'})
-                bd.append({'value':factorialAprox(value),'op':op + '!'})
+    else:
+
+        if not any(elm['value'] == math.sqrt(value) for elm in bd):
+            tree.append({'value':math.sqrt(value), 'op':op + 'V'})
+            bd.append({'value':math.sqrt(value), 'op':op + 'V'})
+
+        if not any(elm['value'] == math.floor(value) for elm in bd):
+            tree.append({'value':math.floor(value),'op':op + '-'})
+            bd.append({'value':math.floor(value), 'op':op + '-'})
+
+        if(value - math.trunc(value) == 0):
+            if not (abs(math.factorial(value)) > (2 ** 31 - 1)):
+                if not any(elm['value'] == math.factorial(value) for elm in bd):
+                    tree.append({'value':math.factorial(value), 'op':op + '!'})
+                    bd.append({'value':math.factorial(value),'op':op + '!'})
+
+            elif (value <= 24):
+                if not any(elm['value'] == factorialAprox(value) for elm in bd):
+                    tree.append({'value':factorialAprox(value), 'op':op + '!'})
+                    bd.append({'value':factorialAprox(value),'op':op + '!'})
 
 
 
